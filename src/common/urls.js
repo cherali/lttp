c0574fd1f0cab1174048944b72cb0e1ea793fe6b
ali cheraghi <alicheraghi148@gmail.com>
2020-11-25 11:34:55 +0330

    -- cleanup

--- src/common/urls.js
+++ src/common/urls.js
@@ -6,7 +6,3 @@ const baseUrl = `${serverUrl}/api/rest/${apiVersion}`
 export const productUrl = `${baseUrl}/get_product`
 
 export const categoryUrl = `${baseUrl}/get_categories`
-
-
-// http://back.dev.liateam.ir/api/rest/v1/get_product?categories=10
-
