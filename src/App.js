e98a7d53ae07ddcbafc27a4da8e874f571d98a69
ali cheraghi <alicheraghi148@gmail.com>
2020-11-25 09:51:59 +0330

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './Containers/Home';
+import Category from './Containers/Category';
+import Products from './Containers/Products';
+
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/category' component={Category} />
+        <Route path='/products' component={Products} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
