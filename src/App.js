969a9928d7384ac9ff368f792b7439bf938c4dc2
ali cheraghi <alicheraghi148@gmail.com>
2020-11-25 11:04:47 +0330

    lazy load containers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from './Containers/Home';
-import Category from './Containers/Category';
-import Products from './Containers/Products';
+
+const Home = lazy(() => import('./Containers/Home'))
+const Products = lazy(() => import('./Containers/Products'))
+const Category = lazy(() => import('./Containers/Category'))
 
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/category' component={Category} />
-        <Route path='/products' component={Products} />
+      <Suspense fallback={null}>
+          <Route exact path='/' component={Home} />
+          <Route path='/category' component={Category} />
+          <Route path='/products' component={Products} />
+        </Suspense>
       </Switch>
     </BrowserRouter>
   );
