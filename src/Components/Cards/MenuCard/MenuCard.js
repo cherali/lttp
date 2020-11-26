04207beabffdf97745a3fe1bb87e1bed442d35e1
ali cheraghi <alicheraghi148@gmail.com>
2020-11-26 13:35:04 +0330

    add active class to menu links

--- src/Components/Cards/MenuCard/MenuCard.js
+++ src/Components/Cards/MenuCard/MenuCard.js
@@ -1,11 +1,15 @@
 import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useQuery } from '../../../hooks/useQuery';
 
 import './menu-card.scss'
 
 function MenuCard({ item }) {
+
+  const id = useQuery('id')
+
   return (
-    <Nav.Link as='li' >
+    <Nav.Link as='li' active={id == item.id} >
       <Link to={`/products?id=${item.id}`}>
         {item.name}
       </Link>
