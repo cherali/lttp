8d9600b4094bab9f227d60387756de79a4eed852
ali cheraghi <alicheraghi148@gmail.com>
2020-11-26 13:05:29 +0330

    add cart icon

--- src/Components/Wrappers/IconWrapper.js
+++ src/Components/Wrappers/IconWrapper.js
@@ -3,12 +3,13 @@ const path = 'assets/images/'
 const ICONS = {
   chevronLeft: `${path}chevron-left.svg`,
   cartPlus: `${path}cart-plus.svg`,
+  cart: `${path}cart.svg`,
 }
 
 
 const IconWrapper = ({ name, className }) => {
   return (
-    <img src={ICONS[name]} alt={name} className={className}/>
+    <img src={ICONS[name]} alt={name} className={className} />
   )
 }
 
